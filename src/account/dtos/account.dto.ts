ea200e4e09a9e3e4b47ec376bd82d1bd48617b2a
irenehl <26irenelopez@gmail.com>
2023-05-16 09:03:29 -0600

    feat: add account dtos

---
+++ src/account/dtos/account.dto.ts
@@ -0,0 +1,3 @@
+import { Account } from '@prisma/client'
+
+export type AccountDto = Omit<Account, 'password'>
---
+++ src/account/dtos/create-account.dto.ts
@@ -0,0 +1,6 @@
+import { Prisma } from '@prisma/client'
+
+export type CreateAccountDto = Omit<
+    Prisma.AccountCreateInput,
+    'createdAt' | 'updatedAt' | 'role'
+>
