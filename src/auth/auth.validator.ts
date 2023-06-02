1941e7245328a35ee40be0e4f544ef671dc0837b
irenehl <26irenelopez@gmail.com>
2023-06-02 09:53:29 -0600

    feat: update auth validator

--- src/auth/auth.validator.ts
+++ src/auth/auth.validator.ts
@@ -4,11 +4,3 @@ export const loginSchema = joi.object({
     email: joi.string().email().required(),
     password: joi.string().min(4).alphanum().required(),
 });
-
-// export const emailSchema = joi.object({
-//     email: joi.string().email().required(),
-// });
-
-// export const resetPasswordSchema = joi.object({
-//     password: joi.string().min(4).alphanum().required(),
-// });
